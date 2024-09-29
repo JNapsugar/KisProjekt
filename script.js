063ef4156d12354dfe80679821cec369db47d3fe
JNapsugar <147054654+JNapsugar@users.noreply.github.com>
2024-09-29 17:28:21 +0200

    V3
    Ellenőrzések, jelszó tooltip,
    post get x-x

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function Kor(val,id) {
 }
 //#endregion
 
-//#region Ágazat
+//#region Ágazat vélasztás
 let agazat = "Egészségügy"
 let valasztottAg = false
 let divek = document.getElementsByClassName("agazat")
@@ -26,6 +26,7 @@ for (let i = 0; i < divek.length; i++) {
 //#endregion
 
 //#region Jelszó
+//Láthatóság
 let szem = document.getElementById("jelszoSzem")
 let jelszo = document.getElementById("Jelszo")
 szem.onclick = function () {
@@ -38,7 +39,7 @@ szem.onclick = function () {
     }
 }
 
-
+//Generálás
 const karakterek ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 document.getElementById("generalas").addEventListener("click", function(){
     let generalt = "";
@@ -46,23 +47,28 @@ document.getElementById("generalas").addEventListener("click", function(){
     for ( let i = 0; i < 15; i++ ) {
         generalt += karakterek.charAt(Math.floor(Math.random() * length));
     }
-    jelszo.value = generalt
+    jelszo.value = generalt + Math.floor(Math.random() * 9)
     jelszo.disabled = true
     jelszo.type = "text";
         szem.src = "img/Icon/jelszoLathato.svg"
 
 })
 
+//Saját jelszó
 document.getElementById("sajat").addEventListener("click", function(){
     jelszo.value = ""
     jelszo.disabled = false
 })
 //#endregion
 
-//#region Regisztacio
+//#region Regisztráció
 let mezok = document.getElementsByClassName("textinput")
 let agazatok = document.getElementsByClassName("agazat")
+
 document.getElementById("regisztracioBtn").addEventListener("click", function(){
+    //Ellenőrzések
+    let jelszoInput = document.getElementById("Jelszo").value
+    let emailInput = document.getElementById("Email").value
     let hianyzo = false
     for (let i = 0; i < mezok.length; i++) {
         if (mezok[i].value == ""){
@@ -87,11 +93,55 @@ document.getElementById("regisztracioBtn").addEventListener("click", function(){
     let nem = !document.getElementById('Nő').checked && !document.getElementById('Férfi').checked && !document.getElementById('Egyéb').checked? false : true
     if (hianyzo || !valasztottAg || !nem) {
         document.getElementById("hiba").innerText = "Minden adat kitöltése kötelező!"  
-        console.log(hianyzo)
-        console.log(valasztottAg)
-        console.log(nem);
+    }
+    else if(!/\d/.test(jelszoInput) || !/[A-Z]/.test(jelszoInput) || !/[a-z]/.test(jelszoInput) || jelszoInput.length< 5){
+        document.getElementById("hiba").innerText = "Nem megfelelő jelszó!"  
+    }
+    else if(!(emailInput.includes('@')&&emailInput.includes('.'))){
+        document.getElementById("hiba").innerText = "Nem megfelelő email!"  
     }
     else{
+
+        //POST, GET
+        let nemek = document.getElementsByName('nem');
+        let valasztottNem
+        for (let i = 0; i < nemek.length; i++) {
+            if (nemek[i].checked) {
+                valasztottNem = nemek[i].id
+            }
+            
+        }
+
+        let body={
+            nev: document.getElementById("Nev").value,
+            kor: Number(document.getElementById("Kor").value),
+            nem: valasztottNem,
+            foglalkozas: document.getElementById("Foglalkozás").value,
+            agazat: agazat
+        }
+
+        fetch("index.html",{
+            method : "POST",
+            body : JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json'}
+            })
+
+        let felhasznalok = []
+        fetch("index.html")
+            .then(
+                function(data) {
+                    return data.json()
+                } 
+            )
+            .then(
+                function(data) {
+                    for (let i = 0; i < data.length; i++) {
+                        felhasznalok.push(data[i])
+                    }
+                }
+            )
+        console.log(felhasznalok)      
         document.getElementById("content").innerHTML = '<h2 id="sikeres">Sikeres regisztráció</h2>'
     }
 
@@ -99,3 +149,4 @@ document.getElementById("regisztracioBtn").addEventListener("click", function(){
 
 
 //#endregion 
+
